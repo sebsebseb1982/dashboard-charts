3e9a42ecd06e3fe8f0e92cac4e81226b203ae5de
Seb <titi@toto.fr>
2020-11-30 16:23:47 +0100

    nettoyage

--- src/Api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var port = 8012;
-app_1.default.listen(port, function () {
-    console.log("L'API \u00E9coute sur le port " + port);
-});
